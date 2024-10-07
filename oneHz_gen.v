e2257ac098883d358cec7d8e916ee8c96bd0c9c4
Rudra Pratap Rohan <126269895+RudraPratapRohan@users.noreply.github.com>
2024-10-07 11:39:12 +0530

    Update oneHz_gen.v
    Signed-off-by: Rudra Pratap Rohan <126269895+RudraPratapRohan@users.noreply.github.com>

--- oneHz_gen.v
+++ oneHz_gen.v
@@ -1,3 +1,4 @@
+//The oneHz_gen module divides the 100 MHz input clock down to 1 Hz.
 `timescale 1ns / 1ps
 
 module oneHz_gen(
@@ -14,9 +15,10 @@ module oneHz_gen(
             clk_reg <= ~clk_reg;
         end
         else
-            counter_reg <= counter_reg + 1;
+            counter_reg <= counter_reg + 1;//The counter counts up to 49,999,999 (representing 0.5 seconds) and toggles the output clock.
     end
     
     assign clk_1Hz = clk_reg;
-    
+//the output clock (clk_1Hz) toggles every 0.5 seconds, resulting in a full cycle (0 -> 1 -> 0) every 1 second, 
+    //which generates a clock signal with a frequency of 1 Hz.    
 endmodule
