1ea899f65fdd085e9934b4ca14147a2400464372
Rudra Pratap Rohan <126269895+RudraPratapRohan@users.noreply.github.com>
2024-10-07 11:37:11 +0530

    Update towHz_gen.v
    Signed-off-by: Rudra Pratap Rohan <126269895+RudraPratapRohan@users.noreply.github.com>

--- towHz_gen.v
+++ towHz_gen.v
@@ -1,3 +1,6 @@
+//so fpga has high frequency and if we give input it will take multiple input so avoid that we use frequency divider
+//The twoHz_gen module divides a 100 MHz clock signal down to a 2 Hz signal T = 0.5sec
+
 `timescale 1ns / 1ps
 
 module twoHz_gen(
@@ -9,7 +12,7 @@ module twoHz_gen(
     reg clk_reg = 0;
     
     always @(posedge clk_100MHz) begin
-        if(counter_reg == 24_999_999) begin
+        if(counter_reg == 24_999_999) begin//The counter counts to 24,999,999 (representing 0.25 seconds) and toggles the output clock.
             counter_reg <= 0;
             clk_reg <= ~clk_reg;
         end
@@ -17,6 +20,7 @@ module twoHz_gen(
             counter_reg <= counter_reg + 1;
     end
     
-    assign clk_2Hz = clk_reg;
+    assign clk_2Hz = clk_reg;//The output clock (clk_2Hz) toggles every 0.25 seconds,
+    //resulting in a full cycle (0 -> 1 -> 0) every 0.5 seconds, creating a clock with a frequency of 2 Hz.
     
 endmodule
